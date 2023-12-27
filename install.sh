c050162fba16d7210d7980509fe98c10a8c90b43
Andrew Hanes <andrew.hanes@datadoghq.com>
2023-12-27 13:08:38 -0500

    init

---
+++ install.sh
@@ -0,0 +1,3 @@
+apt-get install -y clang curl zsh
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
