2473aba9f873bdb0933e04f44fdfd2dd5050f256
Andrew Hanes <andrew.hanes@datadoghq.com>
2024-12-03 11:12:03 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,3 @@ touch ~/.zshrc
 apt-get install -y clang curl zsh
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-echo "source ~/.sdkman/bin/sdkman-init.sh" >> ~/.zshrc
-curl -L https://raw.githubusercontent.com/rust-lang/rustlings/main/install.sh | bash
-
